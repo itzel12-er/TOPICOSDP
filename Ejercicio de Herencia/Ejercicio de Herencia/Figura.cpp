cdebcd03b76fdafc1efa6b84f7580b26b7944e8d
itzel12-er <naty.jurica@gmail.com>
2025-07-12 13:29:54 -0600

    correcciones
    virtuales puras correccion

---
+++ Ejercicio de Herencia/Ejercicio de Herencia/Figura.cpp
@@ -0,0 +1,7 @@
+
+#include "Figura.h"
+
+// Devuelve el nombre de la figura de forma segura
+const std::string& Figura::ObtenerNombreDeFigura() const {
+    return nombre;
+}
---
+++ Ejercicio de Herencia/Ejercicio de Herencia/FiguraNLados.cpp
@@ -0,0 +1,18 @@
+﻿
+#include "FiguraNLados.h"
+
+// Constructor que pasa "FiguraNLados" a la clase base
+FiguraNLados::FiguraNLados(int n, float l) : Figura("FiguraNLados"), nLados(n), longitudLado(l) {}
+
+// Destructor vacío, no hay memoria dinámica que liberar
+FiguraNLados::~FiguraNLados() = default;
+
+// Cálculo del área de un polígono regular: (n * lado²) / (4 * tan(π/n))
+float FiguraNLados::CalcularArea() {
+    return (nLados * longitudLado * longitudLado) / (4 * tan(3.14159f / nLados));
+}
+
+// Cálculo del perímetro de un polígono regular: n * lado
+float FiguraNLados::CalcularPerimetro() {
+    return nLados * longitudLado;
+}
